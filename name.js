30385ce3302b94bc7f55214cbe9a8be3c76f019b
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-02-28 02:38:12 +0900

    Add files via upload

--- name.js
+++ name.js
@@ -10,7 +10,7 @@ $(function () {
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\nお名前：${name}\nご連絡先：${tel}`;
+        var msg = `お名前：${name}\nご連絡先：${tel}`;
         sendText(msg);
 
         return false;
